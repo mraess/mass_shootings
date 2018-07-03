767d478a53a3c7d10c2c402d493836427b3a153c
Primesty <matthias.raess@gmail.com>
2018-07-03 13:44:56 -0400

    init

---
+++ mass_preprocessing.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(conflicted)
+library(shinymaterial)
+library(googlesheets)
+library(magrittr)
+
+filter <- dplyr::filter
+
+# register googlesheet from "Mother Jones - Mass Shootings Database 1982 - 2018
+
+gs_mass_shootings <- gs_url(x = "https://docs.google.com/spreadsheets/d/1b9o6uDO18sLxBqPwl_Gh9bnhW-ev_dABH83M5Vb5L8o/edit#gid=0")
+
+gs_ws_ls(gs_mass_shootings)
+
+## Get data out of registered gs object
+
+mass_shootings <- gs_mass_shootings %>%
+        gs_read(ws = "Sheet1")
+
+## Data pre-processing
+
+glimpse(mass_shootings)
+
+# Create state and city variables from location
+
+mass_shootings %<>% separate(col = location, into = c("city", "state"), sep = ", ", remove = FALSE)
+
+# Create data variable from date
+
+mass_shootings %<>% mutate(date = as.Date(date, format = "%m/%d/%y"))
+
+## Change variable names with spaces or other symbols to underscore
+
+names(mass_shootings) %<>% str_remove_all(pattern = "-|\\(|\\)") %>% str_replace_all(pattern = "[:space:]", replacement = "_") %>% str_replace_all(pattern = "__", replacement = "_")
+
+
+summary(mass_shootings)
+
+## Clean up character-type variables
+
+mass_shootings %<>% mutate(prior_signs_of_mental_health_issues = str_replace_all(prior_signs_of_mental_health_issues, pattern = "-", replacement = "TBD"), 
+                           mental_health_details =  str_replace_all(mental_health_details, pattern = "-", replacement = "TBD"),
+                           weapons_obtained_legally = str_replace_all(weapons_obtained_legally, pattern = "-", replacement = "TBD"),
+                           where_obtained = str_replace_all(where_obtained, pattern = "-", replacement = "TBD"),
+                           weapon_details = str_replace_all(mass_shootings$weapon_details, pattern = "^-$", replacement = "Unknown"),
+                           weapons_obtained_legally = str_replace_all(weapons_obtained_legally, pattern = 'Yes \\(\\".+\\"\\)|^\\nYes', replacement = "Yes"),
+                           weapons_obtained_legally = str_replace_all(weapons_obtained_legally, pattern = 'Kelley+', replacement = "Passed federal background check"))
+
+
+
+
